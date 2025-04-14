66e96a2c7c4a4e60bd8abe9e59bb3c1de11a9fe5
VaibhavK <vaibhavkrkandhway@gmail.com>
2025-04-14 17:11:37 +0530

    first commit

---
+++ project/src/components/LiveChart.tsx
@@ -0,0 +1,13 @@
+import React, { useEffect, useState } from 'react';
+
+const LiveChart: React.FC = () => {
+  // This is a placeholder component - you'll want to integrate with a charting library
+  // like Chart.js or Recharts for actual implementation
+  return (
+    <div className="w-full h-64 bg-gray-50 rounded-lg flex items-center justify-center">
+      <p className="text-gray-500">Live sensor data chart will be displayed here</p>
+    </div>
+  );
+};
+
+export default LiveChart;
